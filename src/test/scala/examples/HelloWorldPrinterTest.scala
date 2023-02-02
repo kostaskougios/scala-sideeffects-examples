349909ae7f341f43c2ab68c4b6e6b2bf1f38ef33
Kostas Kougios <kostas.kougios@googlemail.com>
2023-02-02 20:10:09 +0000

    -

--- src/test/scala/examples/HelloWorldPrinterTest.scala
+++ src/test/scala/examples/HelloWorldPrinterTest.scala
@@ -1,13 +1,13 @@
 package examples
 
 import org.scalatest.funsuite.AnyFunSuiteLike
-import org.scalatest.matchers.should.Matchers._
+import org.scalatest.matchers.should.Matchers.*
 import sideeffects.console.InMemoryPrintLnLib
 
 class HelloWorldPrinterTest extends AnyFunSuiteLike:
-  val lib = new InMemoryPrintLnLib {}
-
+  def createLib = new InMemoryPrintLnLib {}
   test("it does print hello world!") {
+    val lib = createLib
     new HelloWorldPrinter(lib).helloWorld()
     lib.printed should be(Seq("Hello World!"))
   }
